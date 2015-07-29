ba6ccba48bb428dd2b39cf381d9f84851a440c93
fasteque <daniele.altomare@gmail.com>
2015-07-29 23:45:45 +0200

    Declared first basic model data

---
+++ app/src/main/java/com/fasteque/rxbackup/model/entities/ApplicationInfo.java
@@ -0,0 +1,53 @@
+package com.fasteque.rxbackup.model.entities;
+
+/**
+ * Created by danielealtomare on 29/07/15.
+ * Project: RxBackup
+ */
+public class ApplicationInfo {
+    private String packageName;
+    private String name;
+    private long size;
+    private long lastUpdateTime;
+    private String icon;
+
+    public String getPackageName() {Renam
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(long lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
