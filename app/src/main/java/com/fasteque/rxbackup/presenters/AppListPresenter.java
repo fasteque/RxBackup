ae57a7a5a794ddd511b6e62bc91a12bccccb086f
fasteque <daniele.altomare@gmail.com>
2015-08-02 17:51:36 +0200

    Application list presenter stubs

---
+++ app/src/main/java/com/fasteque/rxbackup/presenters/AppListPresenter.java
@@ -0,0 +1,25 @@
+package com.fasteque.rxbackup.presenters;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+
+/**
+ * Created by danielealtomare on 02/08/15.
+ * Project: RxBackup
+ */
+public class AppListPresenter implements Presenter {
+    @Override
+    public void onPresenterStart() {
+
+    }
+
+    @Override
+    public void onPresenterStop() {
+
+    }
+
+    @Override
+    public void attachView(@NonNull View view) {
+
+    }
+}
