8a5179f32ef0cf2f145b27c708a32b08aff3afe7
fasteque <daniele.altomare@gmail.com>
2015-08-05 21:49:20 +0200

    Defined view interfaces

---
+++ app/src/main/java/com/fasteque/rxbackup/views/AppListView.java
@@ -0,0 +1,8 @@
+package com.fasteque.rxbackup.views;
+
+/**
+ * Created by danielealtomare on 05/08/15.
+ * Project: RxBackup
+ */
+public interface AppListView extends View {
+}
---
+++ app/src/main/java/com/fasteque/rxbackup/views/View.java
@@ -0,0 +1,11 @@
+package com.fasteque.rxbackup.views;
+
+import android.support.annotation.StringRes;
+
+/**
+ * Created by danielealtomare on 05/08/15.
+ * Project: RxBackup
+ */
+public interface View {
+    void displayError(@StringRes int message);
+}
