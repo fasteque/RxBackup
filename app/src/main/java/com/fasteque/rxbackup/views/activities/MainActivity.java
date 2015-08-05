ae3af79e1ea2413385981a546d1f35854cb1f023
fasteque <daniele.altomare@gmail.com>
2015-08-05 22:36:49 +0200

    Minor refactoring

--- app/src/main/java/com/fasteque/rxbackup/views/activities/MainActivity.java
+++ app/src/main/java/com/fasteque/rxbackup/views/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
@@ -99,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements AppListView {
     }
 
     private void initRecyclerView() {
-        // TODO
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     private void initDependencyInjector() {
