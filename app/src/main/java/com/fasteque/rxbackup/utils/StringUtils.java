39fa2db37b3c24dafb8d60f88a1297ad193f4cfd
fasteque <daniele.altomare@gmail.com>
2015-08-09 17:39:58 +0200

    Format utility to convert bytes in a human readable way

---
+++ app/src/main/java/com/fasteque/rxbackup/utils/StringUtils.java
@@ -0,0 +1,22 @@
+package com.fasteque.rxbackup.utils;
+
+/**
+ * Created by danielealtomare on 09/08/15.
+ * Project: RxBackup
+ */
+public class StringUtils {
+
+    /**
+     * Android has it own formater for bytes, but with this utility, there's no need to pass a Context.
+     * In AppListAdapter we do not have to pass a context yet, so let's keep this method.
+     */
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+}
