57e0987227918701317a49f844a4543939ec90a7
fasteque <daniele.altomare@gmail.com>
2015-11-28 21:56:02 +0100

    #7: implemented chrome custom tabs logic

--- app/src/main/java/com/fasteque/rxbackup/views/activities/AttributionsActivity.java
+++ app/src/main/java/com/fasteque/rxbackup/views/activities/AttributionsActivity.java
@@ -1,16 +1,26 @@
 package com.fasteque.rxbackup.views.activities;
 
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.fasteque.rxbackup.R;
+import com.fasteque.rxbackup.helpers.CustomTabActivityHelper;
+import com.fasteque.rxbackup.helpers.WebViewFallback;
 import com.fasteque.rxbackup.views.fragments.AttributionsFragment;
 
 /**
  * Created by danielealtomare on 01/08/15.
  * Project: RxBackup
  */
-public class AttributionsActivity extends AppCompatActivity {
+public class AttributionsActivity extends AppCompatActivity implements AttributionsFragment
+        .OnPreferenceSelectedListener{
+
+    private CustomTabActivityHelper customTabActivityHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +29,23 @@ public class AttributionsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        setupCustomTabHelper();
+
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new AttributionsFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        customTabActivityHelper.bindCustomTabsService(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        customTabActivityHelper.unbindCustomTabsService(this);
     }
 
     @Override
@@ -32,5 +56,38 @@ public class AttributionsActivity extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setupCustomTabHelper() {
+        customTabActivityHelper = new CustomTabActivityHelper();
+        customTabActivityHelper.setConnectionCallback(connectionCallback);
+    }
+
+    private CustomTabActivityHelper.ConnectionCallback connectionCallback = new CustomTabActivityHelper
+            .ConnectionCallback() {
+        @Override
+        public void onCustomTabsConnected() {
+            // Use this callback to perform UI changes.
+        }
+
+        @Override
+        public void onCustomTabsDisconnected() {
+            // Use this callback to perform UI changes.
+        }
+    };
+
+    @Override
+    public void onPreferenceWithUriSelected(Uri uri) {
+        CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intentBuilder.setToolbarColor(getResources().getColor(R.color.primary, getTheme()));
+        } else {
+            //noinspection deprecation
+            intentBuilder.setToolbarColor(getResources().getColor(R.color.primary));
+        }
+        intentBuilder.setShowTitle(true);
+
+        CustomTabActivityHelper.openCustomTab(this, intentBuilder.build(), uri, new WebViewFallback());
     }
 }
