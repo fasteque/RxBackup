e22585dc7cb970d597f5bf579674a57486742bea
fasteque <daniele.altomare@gmail.com>
2015-11-28 21:55:44 +0100

    #7: implemented listener for preferences click

--- app/src/main/java/com/fasteque/rxbackup/views/fragments/AttributionsFragment.java
+++ app/src/main/java/com/fasteque/rxbackup/views/fragments/AttributionsFragment.java
@@ -1,7 +1,11 @@
 package com.fasteque.rxbackup.views.fragments;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.fasteque.rxbackup.R;
 
@@ -10,9 +14,38 @@ import com.fasteque.rxbackup.R;
  * Project: RxBackup
  */
 public class AttributionsFragment extends PreferenceFragment {
+
+    OnPreferenceSelectedListener onPreferenceSelectedListener;
+
+    public interface OnPreferenceSelectedListener {
+        void onPreferenceWithUriSelected(Uri uri);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            onPreferenceSelectedListener = (OnPreferenceSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnPreferenceSelectedListener");
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.attributions);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if(preference.getIntent() != null && preference.getIntent().getData() != null) {
+            onPreferenceSelectedListener.onPreferenceWithUriSelected(preference.getIntent().getData());
+            return true;
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen,preference);
     }
 }
