d237ce985cc0ff736efa8cabe42239080d0c9805
fasteque <daniele.altomare@gmail.com>
2016-02-14 13:54:57 +0100

    Removed LeakCanary initialization

--- app/src/main/java/com/fasteque/rxbackup/RxBackupApplication.java
+++ app/src/main/java/com/fasteque/rxbackup/RxBackupApplication.java
@@ -7,8 +7,6 @@ import android.support.annotation.NonNull;
 import com.fasteque.rxbackup.injection.components.ApplicationComponent;
 import com.fasteque.rxbackup.injection.components.DaggerApplicationComponent;
 import com.fasteque.rxbackup.injection.modules.ApplicationModule;
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by danielealtomare on 28/07/15.
@@ -17,12 +15,6 @@ import com.squareup.leakcanary.RefWatcher;
 public class RxBackupApplication extends Application {
 
     private ApplicationComponent applicationComponent;
-    private RefWatcher refWatcher;
-
-    public static RefWatcher getRefWatcher(@NonNull Context context) {
-        RxBackupApplication application = (RxBackupApplication) context.getApplicationContext();
-        return application.refWatcher;
-    }
 
     public ApplicationComponent getApplicationComponent() {
         return applicationComponent;
@@ -31,7 +23,6 @@ public class RxBackupApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        refWatcher = LeakCanary.install(this);
 
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this)).build();
